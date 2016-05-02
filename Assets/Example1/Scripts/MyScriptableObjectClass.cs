81fd5eb003014dcbb89c441bfbbaf3e84b257a2c
yuandecheng <yuandecheng@yuandechengdeMacBook-Pro.local>
2016-05-02 12:56:47 +0800

    create script files

---
+++ Assets/Example1/Scripts/MyScriptableObjectClass.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyScriptableObjectClass : ScriptableObject
+{
+	public string objectName = "New MyScriptableobject";
+    public bool colorIsRamdom = false;
+    public Color thisColor = Color.white;
+    public Vector3[] spawnPoints;
+}
---
+++ Assets/Example1/Scripts/UseMyScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UseMyScriptableObject : MonoBehaviour 
+{
+	void Start () 
+	{
+	
+	}
+}
